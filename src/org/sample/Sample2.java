2f3cd93e08dbad110eed4a46b76cf12dd6c95e06
mohan150791 <flykarma@gmail.com>
2025-01-01 23:03:14 +0530

    Changes done

--- src/org/sample/Sample2.java
+++ src/org/sample/Sample2.java
@@ -8,7 +8,7 @@ public class Sample2 {
 		int[] a= {10, 20, 30, 40, 50, 10, 20, 30};//{ 10,10,20,20,30,30,40,50}
 		Arrays.sort(a);
 		System.out.println(a);
-		System.out.println(a[7]);
+		System.out.println(a[5]);
 		System.out.println("Length of an array: "+a[a.length-1]);
 		int length = a.length;
 		System.out.println(length);
